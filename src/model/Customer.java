f5b68a2bd4b2e0ea3b75b02a28916ad7378a5174
dgutierres26 <diegogutierres@hotmail.com>
2020-05-02 22:00:22 +0100

    customer update

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,17 +1,34 @@
 package model;
 
-class Customer {
-	private String name;
+public class Customer {
+	private String FName;
+	private String LName;
+	private String Email;
 	private int id;
+	private int debitcard;
+	private int creditcard;
 	
-	public Customer setName(String name) {
-		this.name = name;
+	public Customer setFName(String FName) {
+		this.FName = FName;
 		return this;
 	}
-	public String getName() {
-		return this.name;
+	public String getFName() {
+		return this.FName;
 	}
-	
+	public Customer setLName(String LName) {
+		this.LName = LName;
+		return this;
+	}
+	public String geLName() {
+		return this.LName;
+	}
+	public Customer setEmail(String Email) {
+		this.Email = Email;
+		return this;
+	}
+	public String getEmail() {
+		return this.Email;
+	}
 	public Customer setId(int id) {
 		this.id = id;
 		return this;
@@ -19,9 +36,24 @@ class Customer {
 	public int getId() {
 		return this.id;
 	}
+	public Customer setDebitCard(int debitcard) {
+		this.debitcard = debitcard;
+		return this;
+	}
+	public int getDebitCard() {
+		return this.debitcard;
+	}
+	public Customer setCreditCard(int creditcard) {
+		this.creditcard = creditcard;
+		return this;
+	}
+	public int getCreditCard() {
+		return this.creditcard;
+	}
+	
 	@Override
 	public String toString() {
-		return "Customer [name=" + name + ", id=" + id + "]";
+		return "Customer [FName=" + FName + ",LName=" + LName + ", id=" + id + "]";
 	}
 }
 
