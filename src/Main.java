02d820ab60b6518919138bb728243947dfe0d278
lgutodias <dias.luizaugusto@gmail.com>
2020-05-03 01:05:40 +0100

    classes update 8

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
 import java.util.List;
 import model.Album;
+import model.BoxSet;
 import model.Customer;
 import model.Keyboard;
+import model.LiveConcertVideo;
+import model.Movie;
 import model.Titles;
 
 public class Main {
@@ -11,10 +14,15 @@ public class Main {
 	static List<Customer> customers = new ArrayList<>();
 	
 	
-	
 	public static void main(String[] args) {
 		
 		boolean running = false;
+		
+		/*titles.add(makeAlbum("Clo", "Diego", "Gay"));
+		titles.add(makeAlbum("LuLu", "Luiz", "Gay"));
+		titles.add(makeLiveConcertVideo("True man", "Raul", "Male"));
+		titles.add(makeMovie("Bravo", "John", "comedy"));
+		titles.add(makeBoxSet("1", "Lost", "Suspence"));*/
 		
 		System.out.println("=====::::::::::| ULTRA VISION |::::::::::=====");
 		System.out.println("(1) ADD A TITLE");
@@ -27,8 +35,8 @@ public class Main {
 			switch (option) {
 			case "1":
 				//Create Music
-				String artist = Keyboard.textInput("ENTER ALBUM NAME: ");
-				String title = Keyboard.textInput("ENTER ARTIST: ");
+				String artist = Keyboard.textInput("ENTER ARTIST/BAND: ");
+				String title = Keyboard.textInput("ENTER TITLE: ");
 				String genre = Keyboard.textInput("ENTER GENRE: ");
 				int year = Keyboard.numberInput("ENTER YEAR OF RELEASE: ");
 				String media = Keyboard.textInput("ENTER MEDIA FORMAT: ");
@@ -55,13 +63,41 @@ public class Main {
 			running = (Keyboard.textInput("Enter (y/Y) to leave: ").equalsIgnoreCase("y")) ? true : false;
 		} while(!running);
 		
+		System.out.println("========== Title ============");
+		displayTitle(titles);
+		System.out.println("========== Customer ============");
+		displayCustomer(customers);
 		
+	}
+	
+	public static void displayTitle(List<Titles> p) {
+		for (Titles t : p) {
+			System.out.println(t);
+		}
+	}
+	
+	public static void displayCustomer(List<Customer> customer) {
+		for (Customer c : customer) {
+			System.out.println(c);
+		}
 	}
 	
 	public static Album makeAlbum(String artist, String title, String genre,
 			int year, String media) {
 		return (Album) new Album().setArtist(artist).setTitle(title)
 				.setGenre(genre).setYearOfRelease(year).setMediaFormat(media);
+	}
+	
+	public static LiveConcertVideo makeLiveConcertVideo(String art, String name, String gen) {
+		return  (LiveConcertVideo) new LiveConcertVideo().setArtist(art).setName(name).setGenre(gen);
+	}
+	
+	public static Movie makeMovie(String dir, String name, String gen) {
+		return (Movie) new Movie().setDirector(dir).setName(name).setGenre(gen);
+	}
+	
+	public static BoxSet makeBoxSet(String sea, String name, String gen) {
+		return (BoxSet) new BoxSet().setSeason(sea).setName(name).setGenre(gen);
 	}
 	
 	public static Customer makeCustomer(String fname, String lname,
